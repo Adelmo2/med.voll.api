08f631493efb995041ba17719c91f2af4bb51aa6
Adelmo Ricardo <adelmo.ricardo@sherwin-auto.com.br>
2026-01-30 05:52:27 -0300

    30-01-2026

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -20,22 +22,14 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    //public void cadastrar(@RequestBody String json) {
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
-//        System.out.println(dados);
-//        System.out.println("\n-------");
-//        System.out.println("Nome: " + dados.nome());
-//        System.out.println("CRM: " + dados.crm());
-//        System.out.println("E-MAIL: " + dados.email());
-//        System.out.println("ESPECIALIDADE: " + dados.especialidade());
-//        System.out.println("LOGRADOURO: " + dados.endereco().logradouro());
-//        System.out.println("BAIRRO: " + dados.endereco().bairro());
-//        System.out.println("CEP: " + dados.endereco().cep());
-//        System.out.println("CIDADE: " + dados.endereco().cidade());
-//        System.out.println("COMPLEMENTO: " + dados.endereco().complemento());
-//        System.out.println("NUMERO: " + dados.endereco().numero());
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dados);
+        //repository.save(new Medico(dados));
+        repository.save(medico);
 
-        repository.save(new Medico(dados));
+        var uri = uriBuilder.path("/medidcos/{id}").buildAndExpand(medico.getId()).toUri() ;
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoMedico(medico));
     }
 
 //    @GetMapping
@@ -47,10 +41,10 @@ public class MedicoController {
     //http://localhost:8080/medicos?size=1&page=4
     //http://localhost:8080/medicos?sort=especialidade,crm,desc
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        //return repository.findAll(paginacao).map(DadosListagemMedico::new);
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
-        //return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+        return ResponseEntity.ok(page);
+
     }
 
     //*******Para lista todos os campos que estão na classe model ou no banco que tem a entity.*****
@@ -61,16 +55,28 @@ public class MedicoController {
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualisarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         //repository.deleteById(id);
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 }
