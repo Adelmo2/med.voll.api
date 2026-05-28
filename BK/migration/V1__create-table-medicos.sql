e744870cff76727909a4a5a05631ed0150c547e5
Adelmo Ricardo <adelmo.ricardo@sherwin-auto.com.br>
2026-05-28 19:59:26 -0300

    ok 28-05-2026-B

--- BK/migration/V1__create-table-medicos.sql
+++ BK/migration/V1__create-table-medicos.sql
@@ -1,9 +1,10 @@
-create table pacientes(
+create table medicos(
+
     id bigint not null auto_increment,
     nome varchar(100) not null,
     email varchar(100) not null unique,
-    cpf varchar(14) not null unique,
-    telefone varchar(20) not null,
+    crm varchar(6) not null unique,
+    especialidade varchar(100) not null,
     logradouro varchar(100) not null,
     bairro varchar(100) not null,
     cep varchar(9) not null,
@@ -11,5 +12,7 @@ create table pacientes(
     numero varchar(20),
     uf char(2) not null,
     cidade varchar(100) not null,
+
     primary key(id)
+
 );
